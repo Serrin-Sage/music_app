3b0f18cc6c96380a4b09d33d6eb5692cc8ab58c4
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-06-08 19:59:25 -0400

    adding styling to ipod screen, making a screen on transition

--- music_app/src/components/Screen.jsx
+++ music_app/src/components/Screen.jsx
@@ -1,19 +1,34 @@
+import { useEffect, useState } from "react"
+
 const Screen = () => {
+  
+  const [screenActive, setScreenActive] = useState(false)
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenActive(true)
+    }, 1000)
+  }, [])
   return (
     <div className="screen-container">
-      <div className="screen-content">
-        <h2>The best music, globally</h2>
-        <div className="subscriber-form">
-          <form>
-            <input type="text"></input>
-            <input type="text"></input>
-          </form>
+      <div className={screenActive ? "screen-content" : "screen-off"}>
+        <div className={screenActive ? "inner-screen-content" : "display-off"} >
+          <h2>The best music, globally</h2>
+          <div className="subscriber-form">
+            <form>
+              <label className="name-label">
+                Subscriber Name:
+                <input type="text"></input>
+              </label>
+              <input type="text"></input>
+            </form>
+          </div>
+          <div>
+            One text, every Friday.<br/>
+            One song, one playlist, one album
+          </div>
+          <button>Subsribe</button>
         </div>
-        <div>
-          One text, every Friday.<br/>
-          One song, one playlist, one album
-        </div>
-        <button>Subsribe</button>
       </div>
       <div className='app-title'>songshare.io</div>
     </div>
