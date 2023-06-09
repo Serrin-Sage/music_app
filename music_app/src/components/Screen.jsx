80c61eebb7a1b77bbc084eb6e63e0f6d7049e6c1
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-06-09 14:29:48 -0400

    added styling to screen container. changed the container to be two separate divs. One for naviagtion and one for content

--- music_app/src/components/Screen.jsx
+++ music_app/src/components/Screen.jsx
@@ -2,35 +2,44 @@ import { useEffect, useState } from "react"
 
 const Screen = () => {
   
-  const [screenActive, setScreenActive] = useState(false)
-  
-  useEffect(() => {
-    setTimeout(() => {
-      setScreenActive(true)
-    }, 1000)
-  }, [])
   return (
     <div className="screen-container">
-      <div className={screenActive ? "screen-content" : "screen-off"}>
-        <div className={screenActive ? "inner-screen-content" : "display-off"} >
-          <h2>The best music, globally</h2>
-          <div className="subscriber-form">
-            <form>
-              <label className="name-label">
-                Subscriber Name:
-                <input type="text"></input>
-              </label>
-              <input type="text"></input>
-            </form>
-          </div>
+      <div className="screen-content">
+        <div className="left-screen-content">
+          <div className='app-title'>songshare.io</div>
+          {/* <h2>The best music, globally</h2> */}
           <div>
+            <div className="user-selection">
+              <div>Listener</div> <span>{`>`}</span>
+            </div>
+            <div className="user-selection">
+              <div>Musician</div> <span>{`>`}</span>
+            </div>
+          </div>
+          {/* <div>
             One text, every Friday.<br/>
             One song, one playlist, one album
-          </div>
-          <button>Subsribe</button>
+          </div> */}
+         
+        </div>
+        <div className="right-screen-content">
+          <div className="subscriber-form">
+              <form>
+                <label className="name-label">
+                  Subscriber Name:
+                  <input type="text" name="user-name" className="user-input"></input>
+                </label>
+                <br/>
+                <label>
+                  Subscriber Phone Number
+                  <input type="text" name="user-number" className="user-input"></input>
+                </label>
+              </form>
+            </div>
+            <button>Subsribe</button>
         </div>
       </div>
-      <div className='app-title'>songshare.io</div>
+      
     </div>
   )
 }
