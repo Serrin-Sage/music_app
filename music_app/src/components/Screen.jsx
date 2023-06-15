a18b0ce63aa1f3bbc26a478e2e28a59ab665b92d
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-06-15 19:40:46 -0400

    added song list, fetching from Google Sheets. No album art currently. Text is not optimized for smaller screens and long song titles disrupt organization.

--- music_app/src/components/Screen.jsx
+++ music_app/src/components/Screen.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
+import Papa from "papaparse"
 import About from "./About"
 import Listener from "./Listener"
 import Musician from "./Musician"
+import SongList from "./SongList"
 
 const Screen = () => {
 
@@ -10,6 +12,22 @@ const Screen = () => {
   const userSelection = (selection) => {
     setScreenFocus(selection)
   }
+
+  const [songList, setSongList] = useState([])
+  
+  useEffect(() => {
+    const getSongList = () => {
+        Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vSHz5TKUkUDs4AfmJ7QU_pGgrlIi8Pmz3L5fQF1osDmTa4z_8EnbPMymySBmSZamLmoBYRAH0HC8mSi/pub?output=csv", {
+            download: true, 
+            header: true,
+            complete: (result) => {
+                setSongList(result.data)
+            }
+        })
+    }
+
+    getSongList()
+  },[])
 
   return (
     <div className="screen-container">
@@ -26,6 +44,9 @@ const Screen = () => {
             <div className={screenFocus === "about" ? "active-tab" : "user-selection"} onClick={() => userSelection("about")}>
               <div>About</div> <span>{`>`}</span>
             </div>
+            <div className={screenFocus === "song_list" ? "active-tab" : "user-selection"} onClick={() => userSelection("song_list")}>
+              <div>Song List</div> <span>{`>`}</span>
+            </div>
           </div>
          
         </div>
@@ -33,6 +54,7 @@ const Screen = () => {
           {screenFocus === "listener" && <Listener /> }
           {screenFocus === "musician" && <Musician />}
           {screenFocus === "about" && <About />}
+          {screenFocus === "song_list" && <SongList songList={songList}/>}
         </div>
       </div>
       
